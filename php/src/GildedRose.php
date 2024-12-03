dd963b1b17fa2557e867e9610d9422285fd1f3de
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2024-12-03 14:51:20 +0100

    extract stuff

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -20,21 +20,21 @@ final class GildedRose
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                        $this->decreaseQuality($item);
                     }
                 }
             } else {
                 if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                    $this->increaseQuality($item);
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sellIn < 11) {
                             if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                                $this->increaseQuality($item);
                             }
                         }
                         if ($item->sellIn < 6) {
                             if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                                $this->increaseQuality($item);
                             }
                         }
                     }
@@ -50,18 +50,33 @@ final class GildedRose
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
                             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
+                                $this->decreaseQuality($item);
                             }
                         }
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $this->resetQuality($item);
                     }
                 } else {
                     if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                        $this->increaseQuality($item);
                     }
                 }
             }
         }
+    }
+
+    private function decreaseQuality(Item $item): void
+    {
+        $item->quality = $item->quality - 1;
+    }
+
+    private function increaseQuality(Item $item): void
+    {
+        $item->quality = $item->quality + 1;
+    }
+
+    private function resetQuality(Item $item): void
+    {
+        $item->quality = $item->quality - $item->quality;
     }
 }
